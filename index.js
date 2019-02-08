0a18108cae978858de89fe3f854cbb9a73abdd3e
FlooDaaFy <46968139+FlooDaaFy@users.noreply.github.com>
2019-02-08 20:53:58 +0100

    Add files via upload
    Il risque d'y avoir des changements

---
+++ index.js
@@ -0,0 +1,68 @@
+$('.cheville').click(function(){
+  $('.textcheville').toggle('slow');
+   $('.textcuisse').hide();
+    $('.texthanche').hide(); 
+     $('.textpoignee').hide();
+      $('.textcoude').hide()
+       $('.textepaule').hide()
+
+
+});
+
+
+$('.hanche').click(function(){
+  $('.texthanche').toggle('slow');
+   $('.textcheville').hide();
+    $('.textcuisse').hide();
+     $('.textpoignee').hide();
+      $('.textcoude').hide()
+        $('.textepaule').hide()
+
+
+});
+
+
+$('.cuisse').click(function(){
+  $('.textcuisse').toggle('slow');
+   $('.textcheville').hide();
+    $('.texthanche').hide();
+     $('.textpoignee').hide();
+      $('.textcoude').hide()
+        $('.textepaule').hide()
+
+});
+
+
+$('.poignee').click(function(){
+  $('.textpoignee').toggle('slow');
+   $('.textcheville').hide();
+    $('.texthanche').hide();
+     $('.textcuisse').hide()
+      $('.textcoude').hide()
+       $('.textepaule').hide()
+});
+
+
+$('.coude').click(function(){
+  $('.textcoude').toggle('slow');
+   $('.textcheville').hide();
+    $('.texthanche').hide();
+     $('.textcuisse').hide()
+      $('.textpoignee').hide()
+       $('.textepaule').hide()
+
+
+});
+
+$('.epaule').click(function(){
+  $('.textepaule').toggle('slow');
+   $('.textcheville').hide();
+    $('.texthanche').hide();
+     $('.textcuisse').hide()
+      $('.textpoignee').hide()
+       $('.textcoude').hide()
+
+
+});
+
+
